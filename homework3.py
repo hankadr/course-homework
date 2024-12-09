dc7252fb6706cbaf3d001a450a2d54343689f389
hankadr <hankadrastichova@gmail.com>
2024-12-09 15:58:19 +0100

    jojlk

--- homework3.py
+++ homework3.py
@@ -1,3 +1,4 @@
+
 #Zdravím, vzala jsem task1 z úplně prvního souboru úkolů a updatovala viz níže
 #The user types in three numbers. The program prints the sum or product of these numbers based on the user's choice.
 sum = 0
@@ -7,4 +8,3 @@ for _ in range(3):
     sum += numbers
     product *= numbers 
 print(f'the sum of the numbers is {sum} and the product is equal to {product}')
-    
