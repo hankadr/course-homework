8d7ac0b846b84da10c1368f4d961c15a552f2d01
hankadr <hankadrastichova@gmail.com>
2024-12-17 10:45:18 +0100

    homework

---
+++ homework4.py
@@ -0,0 +1,14 @@
+#task 1 - palindrome
+palindrome = input(f'submit your input: ') #kayak
+if palindrome == palindrome[ : :-1]:
+        print(f'yayy, it is a palindrome')
+else:
+        print('that is not a palindrome, try again')
+
+#task 2 - type text + reserved words, change reserved words from lowercase to uppercase
+#seděla jsem nad tím asi 2 hodiny, nedám to prostě dokupy a AI mi vyhodilo něco, čemu ani nerozumím, takže to tady trapně nebudu dávat:D pardon
+
+#task 3 - input a text and count the number of senteces
+text = input('submit your text: ') #Ema má mámu. Máma má tátu. Táta má psa.
+count = text.count('.')
+print(f'The number of senteces is {count}.')
---
+++ test.py
@@ -0,0 +1,3 @@
+text = input('submit your text: ') #Ema má mámu. Máma má tátu. Táta má psa.
+count = text.count('.')
+print(f'The number of senteces is {count}.')
